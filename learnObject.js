947ac00f9dc8508b47bd9a7208e8f6608ef9d1d7
lee_seunghun <korea51920@gmail.com>
2022-10-10 14:13:42 +0900

    객체 리터럴과 프로피터
    (객체 키와 값을 설정하는 방법 및 추가와 삭제 방법)
    (프로피터는 키와 값이 합쳐진 하나의 단위)

--- learnObject.js
+++ learnObject.js
@@ -4,4 +4,13 @@ let user = {}; //객체 리터럴 문법
 let user = {
     name: "John", // 키: "name", 값: "John"
     age: 30       // 키: "age", 값: 30
+    "likes birds": true // 복수의 단어는 따옴표로 묶어야 한다
 }
+
+//프로피터 값 얻기
+alert(user.name); // John
+alert(user.age); // 30
+
+user.isAdmin = true;
+
+delete user.age;
