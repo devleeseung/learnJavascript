263e057733e79d17635096a6045c01790e74eaa7
lee_seunghun <korea51920@gmail.com>
2022-10-10 10:51:06 +0900

    함수 선언문 vs 함수 표현식 스코프 정리

--- learnFunctionExpression.js
+++ learnFunctionExpression.js
@@ -48,3 +48,65 @@ sayHi("John"); //error!
 let sayHi = function(name){
     alert(`Hello, ${name}`);
 }
+
+let age = prompt("나이를 알려주세요", 18);
+
+//조건에 따라 함수를 선언함
+if(age < 18){
+    function welcome(){
+        alert("안녕!");
+    }
+} else{
+    
+    function welcome(){
+        alert("안녕하세요!");
+    }
+
+}
+
+//함수를 나중에 호출합니다 
+welcome(); // Error: welcome is not defined
+
+let age = 16;
+
+if (age < 18){
+    welcome();              // (실행)
+
+    function welcome(){
+        alert("안녕!");     //함수 선언문은 함수가 선언된 블록 내 어디 에서든 유효
+    }                       
+
+    welcome();              // (실행)
+
+} else {
+    function welcome() {
+        alert("안녕하세요!");
+    }
+}
+
+// 여기는 중괄호 밖이기 때문에 
+// 증괄호 안에서 선언한 함수 선언문을 호출할 수 없습니다.
+
+welcome(); //Error!: welcome is not defined
+
+
+
+let age = prompt("나이를 알려주세요",18);
+
+let welcome;
+
+if (age < 18){
+    welcome = function(){
+        alert("안녕!");  
+    };                   
+
+} else {
+    welcome = function(){
+        alert("안녕하세요!");  
+    };     
+}
+
+// 여기는 중괄호 밖이기 때문에 
+// 증괄호 안에서 선언한 함수 선언문을 호출할 수 없습니다.
+
+welcome();
