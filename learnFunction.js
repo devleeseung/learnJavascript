fe03dee0b02b7e657af7164c8941e648a1eb2d62
lee_seunghun <korea51920@gmail.com>
2022-10-10 09:54:32 +0900

    getPow 함수 작성
    (두 개의 매개변수를 받아 거듭제곱을 반환 하는 함수)

--- learnFunction.js
+++ learnFunction.js
@@ -14,10 +14,27 @@ function checkAge1(age) {
     return (age > 18 || confirm('보호자의 동의를 받으셨나요?'));
   }
 
-// checkAge1(11);
-// checkAge2(11);
-// checkAge3(11);
+checkAge1(11);
+checkAge2(11);
+checkAge3(11);
 
 function getMin(a,b) {
     return a > b ? b : a
 }
+
+getMin(2,5);
+getMin(3,-1);
+getMin(1,1);
+
+function getPow(x,n) {
+    if(n && n > 1){
+        return (x ** n);
+    }
+    else{
+        alert('자연수를 입력하세요');
+    }
+}
+
+let x = prompt('x를 입력하세요');
+let n = prompt('n를 입력하세요');
+getPow(x,n);
