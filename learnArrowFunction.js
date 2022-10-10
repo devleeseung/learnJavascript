54e510b5ae29ddd893f1e076b0b552f6c328d0af
lee_seunghun <korea51920@gmail.com>
2022-10-10 11:11:56 +0900

    화살표 함수로 변경 하는 과제

--- learnArrowFunction.js
+++ learnArrowFunction.js
@@ -40,3 +40,30 @@ let sum = (a,b) => {
 };
 
 alert (sum(1,2)); //3
+
+
+//-----화살표 함수로 변경하기
+
+function ask(question, yes, no){
+    if (confirm(question) yes())
+    else no();
+}
+
+ask(
+    "동의하십니까?",
+    function() {alert("동의하셨습니다.");},
+    function() {alert("취소 버튼을 누르셨습니다.");}
+);
+
+//------------------------
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+
+ask(
+    "동의하십니까?",
+    () => {alert("동의하셨습니다.");},
+    () => {alert("취소 버튼을 누르셨습니다.");}
+);
