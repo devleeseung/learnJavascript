6d6befca65b996b92f49f7c3d4a4371a8061acc0
lee_seunghun <korea51920@gmail.com>
2022-10-10 16:53:00 +0900

    참조에 의한 비교

--- learnReference.js
+++ learnReference.js
@@ -6,3 +6,13 @@ admin.name = "Pete" // admin 참조 값에 의해 변경됨
 
 alert(user.name); // pete가 출력됨 user 참조 값을 이용해 변경사항을 확인함
 
+let a = {};
+let b = a; //참조에 의한 복사
+
+alert( a == b ); // 두 변수는 같은 객체를 참조
+alert( a === b );
+
+let a = {};
+let b = {}; // 독힙된 두 객체
+
+alert( a == b ); // false
